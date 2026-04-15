a4b36a0be9237ff7d8cbe2b57dc0354a25c1af5e
Ephraim Lyon <39858323+EphraimLyon@users.noreply.github.com>
2026-04-15 12:54:44 +0100

    Profile Intelligence Service App

--- intelligence_app/urls.py
+++ intelligence_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProfileCreateView, ProfileDetailView, ProfileListView
-
+##URL Profile links
 urlpatterns = [
     path('profiles/list', ProfileListView.as_view()),
     path('profiles', ProfileCreateView.as_view()),
