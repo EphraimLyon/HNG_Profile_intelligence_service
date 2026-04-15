a3c9734b141f7bc0b330220f6713d77579288141
Ephraim Lyon <39858323+EphraimLyon@users.noreply.github.com>
2026-04-15 12:53:56 +0100

    Interactive app-like components for the Todo with Vanilla Javascript

--- intelligence_app/urls.py
+++ intelligence_app/urls.py
@@ -3,6 +3,6 @@ from .views import ProfileCreateView, ProfileDetailView, ProfileListView
 
 urlpatterns = [
     path('profiles/list', ProfileListView.as_view()),
-    path('profiles/', ProfileCreateView.as_view()),
+    path('profiles', ProfileCreateView.as_view()),
     path('profiles/<uuid:pk>', ProfileDetailView.as_view()),
 ]
